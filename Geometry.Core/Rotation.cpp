1d87fb7db2f27a1d073dc024eda463304eb1dc01
김혜빈 센서개발팀 <dev.5igner1101@gmail.com>
2026-07-13 11:38:51 +0900

    Rotarion에 Rodrigues 추가

--- Geometry.Core/Rotation.cpp
+++ Geometry.Core/Rotation.cpp
@@ -4,3 +4,51 @@
 #include <cmath>
 
 using namespace geometry;
+
+Eigen::Matrix3d Rotation::Rodrigues(const Eigen::Vector3d& axis, double angle)
+{
+	//Matrix3d R = Matrix3d::Identity() + sin(angle) * K + (1 - cos(angle)) * K * K;
+	
+	if (axis.norm() < 1e-12)
+	{
+		return Eigen::Matrix3d::Identity();
+	}
+
+	Eigen::Vector3d normalizedAxis = axis.normalized();
+	Eigen::Matrix3d K = Skew(normalizedAxis);
+	Eigen::Matrix3d R = Eigen::Matrix3d::Identity() + sin(angle) * K + (1.0 - cos(angle)) * K * K;
+
+	return R;
+}
+
+
+Eigen::Matrix3d Rotation::Skew(const Eigen::Vector3d& v)
+{
+//a.cross(b) == Skew(a) * b 3차원 벡터 외적을 행렬곱으로 표현
+//      ┌                 ┐
+//      │  0   -az   ay   │
+//K(a)= │ az    0   -ax   │
+//      │-ay   ax    0    │
+//      └                 ┘
+	Eigen::Matrix3d K;
+
+	K << 0.0, -v.z(), v.y(),
+		 v.z(), 0.0, -v.x(),
+		-v.y(), v.x(), 0.0;
+
+	return K;
+}
+
+bool Rotation::IsRotationMatrix(const Eigen::Matrix3d& R)
+{
+	Eigen::Matrix3d identity = Eigen::Matrix3d::Identity();
+
+	Eigen::Matrix3d result = R.transpose() * R;
+
+	bool orthogonal = result.isApprox(identity, 1e-9);
+
+	bool determinant = abs(R.determinant() - 1.0) < 1e-9;
+
+	return orthogonal && determinant;
+
+}
--- Geometry.Core/Rotation.h
+++ Geometry.Core/Rotation.h
@@ -10,6 +10,7 @@ namespace geometry
 
 		static Eigen::Matrix3d Rodrigues(const Eigen::Vector3d& axis,double angle);
 
+		//3차원 벡터의 외적을 행렬곱으로 표현
 		static Eigen::Matrix3d Skew(const Eigen::Vector3d& v);
 
 		static bool IsRotationMatrix(const Eigen::Matrix3d& R);
